3311d8abd8743db411c71b6dff7aecd035893027
Fernanda Mantilla <fernandamantilla2016@gmail.com>
2024-07-06 10:40:21 -0500

    agregando una duda

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ function validarTarjeta(numeroTarjeta) {
         let digito = digitos[i];
 
         // Paso 4: Multiplicar por 2 los dígitos en las posiciones impares. La condicion verifica si el indice o la posición del número es par o impar, si es impar, lo multiplicará por 2
+
+       
         if (i % 2 !== 0) {
             digito *= 2;
 
@@ -23,6 +25,8 @@ function validarTarjeta(numeroTarjeta) {
         }
 
         // Paso 6: Sumar todos los dígitos
+
+         // importante!!! el orden de los números inicia en el indice 0, es decir agregamos un 0 invisible al inicio
         suma += digito;
     }
 
@@ -33,6 +37,15 @@ function validarTarjeta(numeroTarjeta) {
 // Ejemplo de uso:
 let numeroTarjeta = "4556737586899855"; // Ejemplo de número de tarjeta
 let esValida = validarTarjeta(numeroTarjeta);
+
+
+//la suma sería: 4 10 5 12 7 6 7 10 8 12 8 18 9 16 5 10
+//aplicando del algoritmo de luhn el array quedaría:
+// 4+1+5+3+7+6+7+1+8+3+8+9+9+7+5+1 =84 No es modulo 10 pero el algoritmo da como resultado que es una tarjeta válida
+
+//duplicandolos sin tener en cuenta el 0 al inicio para los indices
+//2 5 10 6 14 3 14 5 16 6 16 9 18 8 10 5
+//2+5+1+6+5+3+5+5+7+6+7+9+9+8+1+5=84 igual.
 
 if (esValida) {
     console.log(`El número de tarjeta ${numeroTarjeta} es válido según el algoritmo de Luhn.`);
